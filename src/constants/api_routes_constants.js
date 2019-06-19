f9bf95d668c619fad03998dc970f8a21a557e7d8
Minh Anh NGUYEN <minhanh.ngvn@gmail.com>
2019-06-20 00:31:05 +0200

    Update api routes remote

--- src/constants/api_routes_constants.js
+++ src/constants/api_routes_constants.js
@@ -10,7 +10,7 @@ export const api_routes = process.env.REACT_APP_API_MODE_CONNECT === 'local'
             }
         }
     : {
-        basePath: 'https://epstj5cx6c.execute-api.us-east-1.amazonaws.com/dev/api/v1',
+        basePath: 'https://g28hyeraca.execute-api.us-east-1.amazonaws.com/dev/api/v1',
         users: {
             fb: {
                 auth: '/users/auth/facebook',
